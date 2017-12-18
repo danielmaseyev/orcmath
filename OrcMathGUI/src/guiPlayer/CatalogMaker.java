0cc1de51b24d315b47e3af8ab070e4cee300c02b
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-12-18 15:31:16 -0500

    save stuff

--- OrcMathGUI/src/guiPlayer/CatalogMaker.java
+++ OrcMathGUI/src/guiPlayer/CatalogMaker.java
@@ -1,5 +1,7 @@
 package guiPlayer;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -26,18 +28,42 @@ public class CatalogMaker {
 	public void addNewItem(String name, String genre)
 	{
 		list.add(new Movies(name,genre));	
-		System.out.println("Movie added);
+		System.out.println("Movie added");
 	}
 
 	public static void main(String[] args) {
 		CatalogMaker x = new CatalogMaker();
 		System.out.println(x.getCsvContent());
 		Scanner input = new Scanner(System.in);
-		while(input.nextLine() != "stop");
 		System.out.println("Please enter a movie");
 		String name = input.nextLine();	
 		System.out.println("Now add a genre");
 		String genre  = input.nextLine();
+		testSaveContent("test.csv");
+	}
+	
+	private static void testSaveContent(String fileName) {
+
+		try{    
+
+			FileWriter fw=new FileWriter(fileName);    
+
+			for(name m: Movies)
+			{
+			fw.write(m+ "\n");    
+			}
+			fw.close();    
+
+			System.out.println("Success! File \""+fileName+"\" saved!");
+
+		}catch(IOException e){
+
+			System.out.println("An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
+
+		}
+
+
+
 	}
 
 }
